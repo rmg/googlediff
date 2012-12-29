748f24903e4bfdba89da0ca6c42959c88071fa3c
Ryan Graham <r.m.graham@gmail.com>
2012-12-28 22:01:53 -0800

    Cleanup test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var assert = require('assert');
-var diff_match_patch=require('./index');
+var diff_match_patch = require('./index');
 var dmp = new diff_match_patch();
 
 console.dir(diff_match_patch);
@@ -7,25 +7,16 @@ console.dir(diff_match_patch);
 function launch() {
   var text1 = "this is some test. blah blah blah";
   var text2 = "this is other text. blah blah blah";
-  dmp.Diff_Timeout = 1;
 
-  var ms_start = (new Date()).getTime();
-  var d = dmp.diff_main(text1, text2);
-  var ms_end = (new Date()).getTime();
+  var diffs = dmp.diff_main(text1, text2);
 
-  if (true) {
-    dmp.diff_cleanupSemantic(d);
-  }
-  if (false) {
-    dmp.Diff_EditCost = 4;
-    dmp.diff_cleanupEfficiency(d);
-  }
-  //var ds = dmp.diff_prettyHtml(d);
-  //console.log(ds + '<br/>Time: ' + (ms_end - ms_start) / 1000 + 's');
-  console.log(d);
+  dmp.diff_cleanupSemantic(diffs);
 
-  var patch = dmp.patch_make(d);
+  console.log(diffs;
+
+  var patch = dmp.patch_make(diffs);
   var results = dmp.patch_apply(patch, text1);
   assert.equal(results[0], text2);
 }
-launch()
+
+launch();
